48ff53c4ff8cf0e4440b507d1aa86800bad49c23
emil624g <ekkr23@student.aau.dk>
2026-03-09 11:48:45 +0100

    fixed unit tests

--- tests/test_TemporalClassifier.py
+++ tests/test_TemporalClassifier.py
@@ -5,18 +5,23 @@ import torch.nn.functional as Function
 
 from src.GNN.TemporalClassifier import TemporalClassifier
 
+NUM_TIMESTEPS = 20
 
+INPUT_DIM = 11
+HIDDEN_DIM = 64
+OUTPUT_DIM = 3
 
+LEARNING_RATE = 1e-3
 
 def test_output_shape():                                                                        #Shape tests
-    model = TemporalClassifier(input_dim=11, hidden_dim=64, output_dim=3)
+    model = TemporalClassifier(input_dim = INPUT_DIM, hidden_dim = HIDDEN_DIM, output_dim = OUTPUT_DIM, num_timesteps=NUM_TIMESTEPS)
     graph = torch.load(os.path.join("tests/test_data/data-0002-0001.pt"), weights_only=False)
     logits = model(graph)
 
     assert logits.shape == (1, 3), f"Expected [1, 3], got {logits.shape}"
 
 def test_forward_pass():                                                                        #Forward pass doesn't crash 
-    model = TemporalClassifier(input_dim=11, hidden_dim=64, output_dim=3)
+    model = TemporalClassifier(input_dim = INPUT_DIM, hidden_dim = HIDDEN_DIM, output_dim = OUTPUT_DIM, num_timesteps=NUM_TIMESTEPS)
     graph = torch.load(os.path.join("tests/test_data/data-0002-0001.pt"), weights_only=False)
     try:
         logits = model(graph)
@@ -24,13 +29,13 @@ def test_forward_pass():                                                        
         assert False, f"Forward pass failed: {e}"
 
 def test_model_can_overfit():                                                                    #Loss decreases
-    model = TemporalClassifier(input_dim=11, hidden_dim=64, output_dim=3)
+    model = TemporalClassifier(input_dim = INPUT_DIM, hidden_dim = HIDDEN_DIM, output_dim = OUTPUT_DIM, num_timesteps=NUM_TIMESTEPS)
     graph = torch.load(os.path.join("tests/test_data/data-0002-0001.pt"), weights_only=False)
-    graph.y = torch.tensor([1])
-    opt = torch.optim.Adam(model.parameters(), lr=1e-2)
+    graph.label = torch.tensor([1])
+    opt = torch.optim.Adam(model.parameters(), lr = LEARNING_RATE)
 
     for _ in range(100):
-        loss = Function.cross_entropy(model(graph), graph.y)
+        loss = Function.cross_entropy(model(graph), graph.label)
         opt.zero_grad()
         loss.backward()
         opt.step()
