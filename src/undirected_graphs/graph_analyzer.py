e948f136c3b07031e927fb8e636836376ec9659f
emcharif <136385459+emcharif@users.noreply.github.com>
2026-03-13 13:50:14 +0100

    search graph now also returns the length of the leaf-chains

--- src/undirected_graphs/graph_analyzer.py
+++ src/undirected_graphs/graph_analyzer.py
@@ -3,51 +3,65 @@ from utils import UtilityFunctions
 class GraphAnalyzer:    
     def main(self):
         dataset = UtilityFunctions.load_dataset(root="data/REDDIT-BINARY", name="REDDIT-BINARY")
-        graph = dataset[1]
+        graph = dataset[0]
         self.search_graph(graph)
 
     def search_graph(self, graph):
-        """Takes a single graph as a paramter
-        
-        Keyword arguments:
-        argument -- description
-        Return: return_description
-        """
-        
         total_edges = graph.edge_index
         source_nodes_list = total_edges[0].tolist()
         dist_nodes_list = total_edges[1].tolist()
-
-        dangling_nodes = 0
         nodes_that_contain_dangling_chains = []
-        
-        # List of tuples containing edges between two nodes e.g [(source_0, dist_0), (source_1, dist_1) ... (source_n, dist_n)]
+        dangling_nodes_distribution = {}
         edges = []
-        
-        # Dictionary containing a key, value pair where key = node_index and value = amount of edges.
         count_of_edges = {}
 
-        
         for index in range(len(source_nodes_list)):
             source_node = source_nodes_list[index]
             dist_node = dist_nodes_list[index]
             edges.append((source_node, dist_node))
 
-        for edge in edges:
-            value = edge[0]
-            # The .get(value, 0) handles the case where the key doesn't exist yet, defaulting to 0 before adding 1.
-            count_of_edges[value] = count_of_edges.get(value, 0) + 1
+        adjacency = {}
+        for source, dest in edges:
+            if source not in adjacency:
+                adjacency[source] = set()
+            if dest not in adjacency:
+                adjacency[dest] = set()
+            adjacency[source].add(dest)
+            adjacency[dest].add(source)
+
+        for node in adjacency:
+            count_of_edges[node] = len(adjacency[node])
 
         for index in count_of_edges:
-            if count_of_edges[index] == 1: nodes_that_contain_dangling_chains.append(index)
+            if count_of_edges[index] == 1:
+                nodes_that_contain_dangling_chains.append(index)
+
+        # Walk inward from each leaf until a branching node
+        for leaf in nodes_that_contain_dangling_chains:
+            length = 0
+            current = leaf
+            previous = None
+
+            while True:
+                length += 1
+                if count_of_edges[current] != 2:
+                    break
+                next_node = next(n for n in adjacency[current] if n != previous)
+                previous = current
+                current = next_node
+
+            dangling_nodes_distribution[leaf] = length
+
+        print(dangling_nodes_distribution)
 
         return {
-            "graph_index": graph,
+            "graph": graph,
             "amount_dangling_nodes": len(nodes_that_contain_dangling_chains),
             "list_of_dangling_nodes": nodes_that_contain_dangling_chains,
+            "dangling_nodes_distribution": dangling_nodes_distribution,
             "edges": edges
         }
-    
-    
+
+
 analyzer = GraphAnalyzer()
 analyzer.main()
