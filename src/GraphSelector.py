d455f9439225cf1ab7cbf7718e52cedf246aad62
LukasTN03 <144026846+LukasTN03@users.noreply.github.com>
2026-03-17 14:00:48 +0100

    fixes (#13)
    * fixes
    
    Ruff linter
    fixed tests
    removed old files
    
    * .
    
    * Update requirements.txt
    
    * fixed .yml
    
    * ruff add to requirements.txt
    
    ---------
    
    Co-authored-by: LukasTN03 <DL48WD@student.aau.dk>
    Co-authored-by: emcharif <136385459+emcharif@users.noreply.github.com>

--- src/GraphSelector.py
+++ src/GraphSelector.py
@@ -1,4 +1,3 @@
-import os
 import random
 
 from torch_geometric.datasets import TUDataset
--- src/label.py
+++ src/label.py
@@ -4,7 +4,6 @@
 
 from __future__ import annotations
 
-import os
 import glob
 import csv
 from pathlib import Path
