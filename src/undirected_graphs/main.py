7b122d48574d5b9dc43f4499af18da4b7a60e238
emil624g <ekkr23@student.aau.dk>
2026-03-20 10:15:40 +0100

    main push

--- src/undirected_graphs/main.py
+++ src/undirected_graphs/main.py
@@ -8,18 +8,21 @@ from src.undirected_graphs.utils import UtilityFunctions
 from src.GraphSelector import GraphSelector
 from src.undirected_graphs.graph_analyzer import GraphAnalyzer
 from src.undirected_graphs.inject_chain import inject_chain
+from src.undirected_graphs.GNN.Classifier import Classifier
+from src.undirected_graphs.GNN.Trainer import Trainer
 
 class Main:
     def main(self):
         load_dotenv()
         secret_key = os.getenv("SECRET_KEY")
 
+        # Define the dataset name
+        datasetName = "MUTAG"
+
         # Create instances for the classes that are used
         graphAnalyzer = GraphAnalyzer()
         utilityFunctions = UtilityFunctions()
         graphSelector = GraphSelector(secret_key=secret_key)
-        # Define the dataset name
-        datasetName = "MUTAG"
   
         # load dataset
         dataset = utilityFunctions.load_dataset(root=".data/", name=datasetName)
@@ -38,13 +41,24 @@ class Main:
             watermarked_graphs.append(modified_graph)
 
         # Combine watermarked and unselected graphs to create the complete dataset
-        complete_dataset = watermarked_graphs + unselected_graphs
+        watermarked_dataset = watermarked_graphs + unselected_graphs
+        print("WATERMARKED: ", watermarked_dataset[0])
+        print("UNMARKED: ", dataset[0])
 
-        torch.save(complete_dataset, f"data/{datasetName}_watermarked.pt")
-        print(f"Saved {len(complete_dataset)} graphs to data/{datasetName}_watermarked.pt")
+        torch.save(watermarked_dataset, f"data/{datasetName}_watermarked.pt")
+        print(f"Saved {len(watermarked_dataset)} graphs to data/{datasetName}_watermarked.pt")
 
-            
-            
+        trainer_unmarked = Trainer(dataset_name=datasetName)
+        trainer_unmarked.load_data(dataset=dataset)
+
+        trainer_watermarked = Trainer(dataset_name=datasetName)
+        trainer_watermarked.load_data(dataset=watermarked_dataset)
+
+        trainer_unmarked.train()
+        trainer_unmarked.test_and_save()
+
+        trainer_watermarked.train()
+        trainer_watermarked.test_and_save()
 
 
 if __name__ == "__main__":
