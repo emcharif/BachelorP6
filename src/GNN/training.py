8dfa0c649f9e035b7b87ac85bd56f62ca3042acd
emcharif <136385459+emcharif@users.noreply.github.com>
2026-04-07 12:52:30 +0200

    all tests passed

--- src/GNN/training.py
+++ src/GNN/training.py
@@ -1,7 +1,7 @@
 import torch
 from torch_geometric.loader import DataLoader
 from torch_geometric.datasets import TUDataset
-from GNN.Classifier import Classifier
+from src.GNN.Classifier import Classifier
 import torch.nn.functional as Function
 
 #================= CONSTANTS ============================================
