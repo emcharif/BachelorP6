a95af32c8972bc3e247a5f63a33aeff6f0b3eaef
LukasTN03 <144026846+LukasTN03@users.noreply.github.com>
2026-04-10 13:30:12 +0200

    cleanup
    pathing in tests fix

--- tests/test_load_dataset.py
+++ tests/test_load_dataset.py
@@ -1,8 +1,4 @@
-import sys
-import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
-from src.utils import UtilityFunctions
+from utils import UtilityFunctions
 
 
 def test_load_dataset():
