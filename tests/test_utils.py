321268e5a147df2b20f5d6cedbbe5ba8c06f8643
emil624g <ekkr23@student.aau.dk>
2026-04-28 13:45:02 +0200

    full coverage til utils, fjernet fil og flyttet ind i test utils

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,5 @@
 import torch
+import random
 
 from torch_geometric.data import Data
 from utils import UtilityFunctions
@@ -6,7 +7,7 @@ from utils import UtilityFunctions
 utils = UtilityFunctions()
 
 #========================load_dataset()================
-def test_load_dataset():
+def test_load_dataset_ENZYMES():
     dataset = utils.load_dataset(name = "ENZYMES")
 
     assert dataset[0].x is not None
@@ -17,6 +18,23 @@ def test_load_dataset_no_node_features():
     dataset = utils.load_dataset(name="MUTAG")
 
     assert dataset[0].x[0][0] == 1
+
+def test_load_dataset_IMDB_BINARY():
+    dataset = UtilityFunctions().load_dataset(name="IMDB-BINARY")
+    assert len(dataset) > 0, "Dataset should not be empty"
+    for graph in dataset:
+        assert graph.x is not None, "Node features should not be None"
+        assert graph.x.shape[0] == graph.num_nodes, "Node features should have the same number of rows as nodes"
+
+
+#===========select_dangling_node()============================
+def test_select_dangling_node_always_returns_same_based_on_seeded_rng():
+    rng = random.Random(1234)
+    longest_dangling_node_candidates = [[1,4,3],[2,4,1],[10,4,8],[100,4,98]]
+
+    selected_dangling_node = utils.select_dangling_node(dangling_chain=longest_dangling_node_candidates, rng=rng)
+
+    assert selected_dangling_node == [2,4,1]
 
 
 #=================is_binary()============================================
@@ -33,6 +51,56 @@ def test_dataset_is_not_binary():
     binary = utils.is_binary(dataset)
 
     assert not binary
+
+#=======graphs_to_watermark()=================
+def test_graphs_to_watermark_always_returns_same_based_on_seeded_rng_5pct():
+    rng = random.Random(1234)
+
+    dataset = []
+    for i in range(1, 21):
+        dataset.append(i)
+
+    selected_graphs, unselected_graphs = utils.graphs_to_watermark(dataset=dataset, rng=rng, percentage=0.05)    
+
+    assert selected_graphs == [20]
+    assert unselected_graphs == [14, 5, 10, 17, 8, 7, 9, 18, 19, 6, 13, 16, 12, 11, 2, 3, 1, 4, 15]
+
+def test_graphs_to_watermark_always_returns_same_based_on_seeded_rng_10pct():
+    rng = random.Random(1234)
+
+    dataset = []
+    for i in range(1, 21):
+        dataset.append(i)
+
+    selected_graphs, unselected_graphs = utils.graphs_to_watermark(dataset=dataset, rng=rng, percentage=0.10)    
+
+    assert selected_graphs == [20, 14]
+    assert unselected_graphs == [5, 10, 17, 8, 7, 9, 18, 19, 6, 13, 16, 12, 11, 2, 3, 1, 4, 15]
+
+def test_graphs_to_watermark_always_returns_same_based_on_seeded_rng_20pct():
+    rng = random.Random(1234)
+
+    dataset = []
+    for i in range(1, 21):
+        dataset.append(i)
+
+    selected_graphs, unselected_graphs = utils.graphs_to_watermark(dataset=dataset, rng=rng, percentage=0.20)    
+
+    assert selected_graphs == [20, 14, 5, 10]
+    assert unselected_graphs == [17, 8, 7, 9, 18, 19, 6, 13, 16, 12, 11, 2, 3, 1, 4, 15]
+
+def test_graphs_to_watermark_always_returns_same_based_on_seeded_rng_30pct():
+    rng = random.Random(1234)
+
+    dataset = []
+    for i in range(1, 21):
+        dataset.append(i)
+
+    selected_graphs, unselected_graphs = utils.graphs_to_watermark(dataset=dataset, rng=rng, percentage=0.30)    
+
+    assert selected_graphs == [20, 14, 5, 10, 17, 8]
+    assert unselected_graphs == [7, 9, 18, 19, 6, 13, 16, 12, 11, 2, 3, 1, 4, 15]
+
 
 #=================dif_watermarked_and_benign_graph_edges()================
 def test_dif_watermarked_and_benign_graph_edges():
