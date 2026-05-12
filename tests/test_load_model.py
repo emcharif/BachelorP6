81a1191cabde5a1b5316dfc4f816a44768a616a3
emil624g <ekkr23@student.aau.dk>
2026-05-12 10:03:38 +0200

    slettet unødvendig test til load_model

--- tests/test_load_model.py
+++ tests/test_load_model.py
@@ -23,20 +23,6 @@ def test_return_saved_model_if_path_specified():
 
     assert isinstance(saved_model, Classifier)
 
-def test_trains_and_returns_model_if_path_does_not_exist():
-
-    fake_path = "models/ENZYMES/nonexistent_model.pth"
-    dummy_model = MagicMock(spec=Classifier)
-
-    with patch("load_model.Trainer") as MockTrainer:
-        mock_instance = MagicMock()
-        mock_instance.train.return_value = dummy_model
-        MockTrainer.return_value = mock_instance
-
-        new_model = model_loader.load_model(path=fake_path)
-
-    assert isinstance(new_model, Classifier)
-
     
 #==========identify_dataset()============
 def test_suspect_model_trained_on_ENZYMES():
@@ -62,17 +48,3 @@ def test_suspect_model_trained_on_PROTEINS():
     dataset_name = model_loader.identify_dataset(suspect_model)
 
     assert dataset_name == "PROTEINS"
-
-"""
-def test_suspect_model_trained_on_IMDB_BINARY():
-
-    path = "models/IMDB-BINARY/watermarked_model.pth"
-    with open(path, "rb") as file:
-        file_bytes = file.read()
-
-    suspect_model = model_loader.load_model(file_bytes=file_bytes)
-
-    dataset_name = model_loader.identify_dataset(suspect_model)
-
-    assert dataset_name == "IMDB-BINARY"
-"""
